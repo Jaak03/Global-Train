6a9496804c7c0808041b2f9b364ae54f304cdb76
Jaak <jacques@mohara.co>
2020-05-02 19:33:26 +0200

    [CHANGED] Changed config and the dev site is running locally now.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,94 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  "configureWebpack": (env, argv) => ({
+    mode: argv && argv.mode || 'development',
+    devtool: (argv && argv.mode || 'development') === 'production' ? 'source-map' : 'eval',
+
+    entry: './src/main.js',
+
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].js'
+    },
+
+    node: false,
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'vue-style-loader',
+            'css-loader'
+          ],
+          exclude: /\.module\.css$/
+        }
+      ]
+    },
+
+    resolve: {
+      extensions: [
+        '.js',
+        '.vue',
+        '.json'
+      ],
+      alias: {
+        'vue$': 'vue/dist/vue.esm.js',
+        '@': path.resolve(__dirname, 'src')
+      }
+    },
+
+    plugins: [
+      new CleanWebpackPlugin({
+        cleanAfterEveryBuildPatterns: ['dist']
+      }),
+      new CopyWebpackPlugin([{
+        from: path.resolve(__dirname, 'public'),
+        to: path.resolve(__dirname, 'dist'),
+        toType: 'dir'
+      }])
+    ],
+  
+    
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        minSize: 30000,
+        maxSize: 0,
+        cacheGroups: {
+          vendors: {
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true
+          }
+        }
+      },
+      runtimeChunk: {
+        name: entrypoint => `runtime~${entrypoint.name}`
+      },
+      mangleWasmImports: true,
+      removeAvailableModules: true,
+      removeEmptyChunks: true,
+      mergeDuplicateChunks: true
+    },
+  
+    devServer: {
+      compress: true,
+      host: 'localhost',
+      overlay: true,
+    }
+  })
 }
