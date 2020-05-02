a3825c28209ce393ae285111b21d191171370b37
Jaak <jacques@mohara.co>
2020-05-02 20:04:35 +0200

    [ADDED] Added basic login page.

---
+++ src/components/page/Landing.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Testing now</h1>
+    <Login/>
+  </div>
+</template>
+
+<script>
+  import Login from './Login'
+  export default {
+    name: 'Landing',
+    components: {
+      Login,
+    },
+    data: () => ({
+    })
+  }
+</script>
---
+++ src/components/page/Login.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-card>
+    <v-card-text>
+      <v-form>
+        <v-text-field
+          label="Username"
+          prepend-icon="mdi-account-circle"
+        />
+        <v-text-field
+          label="Password"
+          :type="showPassword ? 'text' : 'password'"
+          prepend-icon="mdi-lock"
+          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          @click:append="toggleShowPassword"
+        />
+      </v-form>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showPassword: false
+    }
+  },
+  methods: {
+    toggleShowPassword() {
+      this.showPassword = !this.showPassword;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
