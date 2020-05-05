84837b5ca453feb7ce932b3a849052fe33fd766b
Jaak <jacques@mohara.co>
2020-05-05 20:27:44 +0200

    [ADDED] Added loading bar on exit instead of snackbar.

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="settings-container">
-    <v-snackbar
+    <!-- <v-snackbar
       v-model="saving"
       color="rgb(0, 45, 87)"
       :timeout="snackbar.timeout"
@@ -26,7 +26,7 @@
           Close
         </v-btn>
       </v-row>
-    </v-snackbar>
+    </v-snackbar> -->
 
     <v-row justify="center" align="center">
       <h1>Settings</h1>
@@ -37,13 +37,12 @@
       class="saved-container"
     >
       <div class="spacer">
-        <v-progress-circular
+        <!-- <v-progress-circular
           v-show="saving"
           indeterminate
           large
-          label="toets"
           color="primary"
-        ></v-progress-circular>
+        ></v-progress-circular> -->
       </div>
     </v-row>
     <v-row justify="center" align="center">
@@ -53,7 +52,7 @@
           :items="session.schedule"
           color="pink"
           label="Sessions"
-          required
+          @change="change()"
         ></v-select>
       </v-col>
     </v-row>
@@ -64,7 +63,7 @@
           :items="session.duration"
           color="pink"
           label="Duration"
-          required
+          @change="change()"
         ></v-select>
       </v-col>
     </v-row>
@@ -72,10 +71,32 @@
       <v-col cols="12" md="6" lg="3">
         <v-checkbox
           v-model="saving"
+          @change="change()"
           label="Receive a notification">
         </v-checkbox>
       </v-col>
     </v-row>
+
+    <v-dialog
+      v-model="saving"
+      hide-overlay
+      persistent
+      width="300"
+    >
+      <v-card
+        color="primary"
+        dark
+      >
+        <v-card-text>
+          Saving your changes.
+          <v-progress-linear
+            indeterminate
+            color="white"
+            class="mb-0"
+          ></v-progress-linear>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
@@ -101,13 +122,28 @@ export default {
         timeourt: 200,
         vertical: true,
       },
-      saving: true
+      next: null,
+      saving: false,
+      changed: false,
     }
   },
   methods: {
-    endNotification() {
-      this.saving = !this.saving;
+    change() {
+      this.changed = true;
+    },
+    async save(next) {
+      this.saving = true;
+      setTimeout(() => {
+        console.log(this.form);
+        this.changed = false;
+        this.saving = false;
+        next();
+      }, 3000);
     }
+  },
+  beforeRouteLeave (to, from , next) {
+    if(this.changed) this.save(next);
+    else next();
   }
 };
 </script>
