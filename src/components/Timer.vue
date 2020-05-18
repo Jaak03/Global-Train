3f92944a73451ef7fa89646b7dd43180af9db22e
Jaak <jacques@mohara.co>
2020-05-18 20:03:45 +0200

    [ADDED] Started working on the timer, but things are not going well.

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,12 +1,39 @@
 <template>
   <div class="timer-container">
-    <p class="timer">TIMER</p>
+    <p class="timer">{{timeString}}</p>
   </div>
 </template>
 
 <script>
+const moment = require('moment');
 export default {
-
+  mounted() {
+    let times = localStorage.getItem('settings').split(',');
+    
+    times = times.map((time) => moment().format(`YYYY-MM-DDT${time}:00`))
+      .sort((a, b) => moment(a) - moment(b));
+    
+    let i = 0;
+    let time;
+    while(moment(times[i]).isBefore(moment()) && i < times.length) {
+      time = moment(times[i]);
+      i += 1;
+    }
+    const current = moment();
+    console.log({ current, time, diff: current - time });
+    this.time = current - time;
+  },
+  data() {
+    return {
+      time: moment(),
+      timeString: ''
+    }
+  },
+  watch: {
+    time: function() {
+      this.timeString = moment(this.time).format('HH:mm:ss');
+    }
+  }
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,6 +14,10 @@ export default {
   },
   components: {
     Timer,
+  },
+  data() {
+    return {
+    }
   }
 }
 </script>
